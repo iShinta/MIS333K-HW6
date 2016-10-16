ca06065113df516d1cb970a92ddfa72342a5abad
iShinta <ho_minh_tri@yahoo.fr>
2016-10-16 10:29:09 -0500

    First Commit

---
+++ Ho_MinhTri_HW6/DAL/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ho_MinhTri_HW6.Models;
+using System.Data.Entity;
+
+namespace Ho_MinhTri_HW6.DAL
+{
+    public class AppDbContext : DbContext
+    {
+        //Constructor that invokes the base constructor
+        public AppDbContext() : base("MyDBConnection") { }
+
+        //Create the db set
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Frequency> Frequencies { get; set; }
+    }
+}
---
+++ Ho_MinhTri_HW6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ho_MinhTri_HW6.MvcApplication" Language="C#" %>
